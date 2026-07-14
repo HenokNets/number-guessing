f5add3ef4f30f82f2366beca3515c42b36d30a2c
henoknets <henoknetsanet98@gmail.com>
2026-07-15 01:04:57 +0300

    refactor: remove the repetitive distance calculation

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let randomNum = Math.floor (Math.random()*50);
 
 const guessBtn = document.querySelector("button");
 
+let distance = Math.abs (userInput - randomNum);
+
 guessBtn.addEventListener ('click', () => {
     let userInput = Number (prompt("Enter your guess"));
     if (userInput === randomNum) {
         console.log("You got the right answer!");
         return;
-    } else if (Math.abs(userInput - randomNum) <= 5) {
+    } else if (distance<= 5) {
         console.log("You're very close!");
-    } else if (Math.abs(userInput - randomNum) <= 10) {
+    } else if (distance <= 10) {
         console.log("You're getting close!");
-    } else if (Math.abs(userInput - randomNum) <= 20) {
+    } else if (distance<= 20) {
         console.log("You're not too far away.");
     } else {
         console.log("You're way off!");
